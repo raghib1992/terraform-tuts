50a457d60a77284e9e0b782b6422fad37c2fb2b3
raghib1992 <raghib.npti@gmail.com>
2024-01-12 22:51:35 +0530

    add test

--- test.tf
+++ test.tf
@@ -1,3 +1,16 @@
 provider "aws"{
   region = "eu-north-1"
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  dynamic "range"{
+    for_each = ["10.0.0.0/24","10.0.1.0/24]
+    content {
+      cidr_block = range.value
+      tags = {
+        Name = "Main"
+      }
+    }
+  } 
+}
