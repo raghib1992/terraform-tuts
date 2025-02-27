f79ccbd774e019195087ac9fcbe170df902a771f
raghib1992 <raghib.npti@gmail.com>
2025-02-28 01:40:38 +0530

    add terragrunt

---
+++ terrgrunt/example/terraform.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = "~> 1.10.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.88.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      "purpose" = "terragrunt"
+    }
+  }
+}
+
+
+locals {
+  name = var.common_name
+  common_tags = {
+    name        = "${local.name}-${var.environment}"
+    environment = var.environment
+  }
+}
