9c582bb05d835d457430a7a2fd4da31677bcb0cc
raghib1992 <raghib.npti@gmail.com>
2025-12-06 14:07:51 +0530

    Add 10

--- Project/aws-eks/08-AWS-EKS-Cluster-Basics/01-ekscluster-terraform-manifests/c3-02-vpc-module.tf
+++ Project/aws-eks/08-AWS-EKS-Cluster-Basics/01-ekscluster-terraform-manifests/c3-02-vpc-module.tf
@@ -18,15 +18,16 @@ module "vpc" {
   private_subnets = var.vpc_private_subnets
 
   # Database Subnets
-  database_subnets = var.vpc_database_subnets
-  create_database_subnet_group = var.vpc_create_database_subnet_group
-  create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
+  # database_subnets = var.vpc_database_subnets
+  # create_database_subnet_group = var.vpc_create_database_subnet_group
+  # create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
   # create_database_internet_gateway_route = true
   # create_database_nat_gateway_route = true
   
   # NAT Gateways - Outbound Communication
-  enable_nat_gateway = var.vpc_enable_nat_gateway 
-  single_nat_gateway = var.vpc_single_nat_gateway
+  # enable_nat_gateway = var.vpc_enable_nat_gateway 
+  # single_nat_gateway = var.vpc_single_nat_gateway
+  enable_nat_gateway = false
 
   # VPC DNS Parameters
   enable_dns_hostnames = true
@@ -48,9 +49,9 @@ module "vpc" {
     "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"    
   }
 
-  database_subnet_tags = {
-    Type = "database-subnets"
-  }
+  # database_subnet_tags = {
+  #   Type = "database-subnets"
+  # }
   # Instances launched into the Public subnet should be assigned a public IP address.
   map_public_ip_on_launch = true
 }
