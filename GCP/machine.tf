b247b2a0a4c50daf0081c3ae685e24b6994d9b7c
raghib1992 <raghib.npti@gmail.com>
2026-02-11 22:58:21 +0530

    add gcp

---
+++ GCP/machine.tf
@@ -0,0 +1,83 @@
+# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
+# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
+
+resource "google_compute_instance" "ubuntu" {
+  boot_disk {
+    auto_delete = true
+    device_name = "ubuntu"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20260114"
+      size  = 10
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src           = "vm_add-tf"
+    goog-ops-agent-policy = "v2-x86-template-1-4-0"
+  }
+
+  machine_type = "e2-medium"
+
+  metadata = {
+    enable-osconfig = "TRUE"
+  }
+
+  name = "ubuntu"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/project-798698c7-9ca3-489f-98a/regions/asia-south1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "770596451333-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  zone = "asia-south1-a"
+}
+
+module "ops_agent_policy" {
+  source          = "github.com/terraform-google-modules/terraform-google-cloud-operations/modules/ops-agent-policy"
+  project         = "project-798698c7-9ca3-489f-98a"
+  zone            = "asia-south1-a"
+  assignment_id   = "goog-ops-agent-v2-x86-template-1-4-0-asia-south1-a"
+  agents_rule = {
+    package_state = "installed"
+    version = "latest"
+  }
+  instance_filter = {
+    all = false
+    inclusion_labels = [{
+      labels = {
+        goog-ops-agent-policy = "v2-x86-template-1-4-0"
+      }
+    }]
+  }
+}
